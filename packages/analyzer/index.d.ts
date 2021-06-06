febc2338997073f61588ae68f9f93054b323cfb9
Benny Powers <web@bennypowers.com>
2021-06-06 13:31:52 +0300

    docs: add typescript typings for plugin authors
    closes #2

---
+++ packages/analyzer/index.d.ts
@@ -0,0 +1,103 @@
+import * as TS from 'typescript'
+import { Module, Package } from 'custom-elements-manifest/schema';
+
+/** Plugin execution context. Pass arbitrary data here. */
+export type Context = Record<string, unknown>;
+
+export interface CollectPhaseParams {
+  /**
+   * TypeScript API
+   */
+  ts: TS;
+
+  /**
+   * The current TypeScript AST Node
+   */
+  node: TS.Node;
+
+  /**
+   * Plugin execution context. Pass arbitrary data here.
+   */
+  context: Context;
+}
+
+export interface AnalyzePhaseParams {
+  /**
+   * TypeScript API
+   */
+  ts: TS;
+
+  /**
+   * The current TypeScript AST Node
+   */
+  node: TS.Node;
+
+  /**
+   * The current state of the current module's manifest
+   */
+  moduleDoc: Partial<Module>;
+
+  /**
+   * Plugin execution context. Pass arbitrary data here.
+   */
+  context: Context;
+}
+
+export interface ModuleLinkPhaseParams {
+  /**
+   * The completed manifest, i.e. the output of the analyze phase
+   */
+  moduleDoc: Module;
+
+  /**
+   * Plugin execution context. Pass arbitrary data here.
+   */
+  context: Context;
+}
+
+export interface PackageLinkPhaseParams {
+  /**
+   * The completed manifest, i.e. the output of the analyze phase
+   */
+  customElementsManifest: Package;
+
+  /**
+   * Plugin execution context. Pass arbitrary data here.
+   */
+  context: Context;
+}
+
+/**
+ * A Custom Elements Manifest Analyzer plugin
+ */
+export interface Plugin {
+  /**
+   * @summary Plugin hook that runs in the analyze phase.
+   *
+   * Runs for all modules in a project, before continuing to the `analyzePhase`
+   */
+  collectPhase(params: CollectPhaseParams): void;
+
+  /**
+   * @summary Plugin hook that runs in the analyze phase.
+   *
+   * Runs for each AST node in each module.
+   * You can use this phase to access a module's AST nodes and mutate the manifest.
+   */
+  analyzePhase(params: AnalyzePhaseParams): void;
+
+  /**
+   * @summary Plugin hook that runs in the module-link phase.
+   *
+   * Post-processing hook that runs for each module, after analyzing.
+   * All information about your module should now be available.
+   */
+  moduleLinkPhase(params: ModuleLinkPhaseParams): void;
+
+  /**
+   * @summary Plugin hook that runs in the package-link phase.
+   *
+   * Runs once per package, after modules have been parsed and after per-module post-processing
+   */
+  packageLinkPhase(params: PackageLinkPhaseParams): void;
+}
