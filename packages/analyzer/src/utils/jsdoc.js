85d7d45ddd0117cd582ada1d966ccac35101e363
thepassle <pascalschilp@gmail.com>
2021-06-29 15:00:26 +0200

    fix: normalize jsdoc starting with a dash

--- packages/analyzer/src/utils/jsdoc.js
+++ packages/analyzer/src/utils/jsdoc.js
@@ -5,3 +5,10 @@
 export function handleJsDocType(type) {
   return type?.replace(/(import\(.+?\).)/g, '') || '';
 }
+
+export function normalizeDescription(desc) {
+  if(desc.startsWith('- ')) {
+    desc = desc.slice(2);
+  }
+  return desc;
+}
