b75dd37a3113a8480d6ac06686c3c5e9d036775d
Benny Powers <web@bennypowers.com>
2021-07-01 13:50:23 +0300

    style(analyzer): pr reviewe comments

--- packages/analyzer/src/features/analyse-phase/creators/createClassField.js
+++ packages/analyzer/src/features/analyse-phase/creators/createClassField.js
@@ -1,6 +1,13 @@
 import ts from 'typescript';
-import { isWellKnownType, isPrimitive } from '../../../utils/ast-helpers.js';
-import { handleModifiers, handleJsDoc, handleTypeInference } from './handlers.js';
+import {
+  handleDefaultValue,
+  handleExplicitType,
+  handleJsDoc,
+  handleModifiers,
+  handlePrivateMember,
+  handleTypeInference,
+  handleWellKnownTypes
+} from './handlers.js';
 
 export function createField(node) {
   let fieldTemplate = {
@@ -8,41 +15,13 @@ export function createField(node) {
     name: node?.name?.getText() || '',
   }
 
-  /**
-   * if is private field
-   * @example class Foo { #bar = ''; }
-   */
-  if (ts.isPrivateIdentifier(node.name)) {
-    fieldTemplate.privacy = 'private';
-  }
-
+  fieldTemplate = handlePrivateMember(fieldTemplate, node);
   fieldTemplate = handleTypeInference(fieldTemplate, node);
-
-  /**
-   * Add TS type
-   * @example class Foo { bar: string = ''; }
-   */
-  if(node.type) {
-    fieldTemplate.type = { text: node.type.getText() }
-  }
-
-  if (isWellKnownType(node)) {
-    const text = node.initializer.expression.getText();
-    fieldTemplate.type = { text };
-    fieldTemplate.default = text;
-  }
-
+  fieldTemplate = handleExplicitType(fieldTemplate, node);
   fieldTemplate = handleModifiers(fieldTemplate, node);
-  fieldTemplate = handleJsDoc(fieldTemplate, node);
   fieldTemplate = handleDefaultValue(fieldTemplate, node);
-
-  return fieldTemplate;
-}
-
-function handleDefaultValue(fieldTemplate, node) {
-  if(isPrimitive(node.initializer)) {
-    fieldTemplate.default = node?.initializer?.getText?.();
-  }
+  fieldTemplate = handleWellKnownTypes(fieldTemplate, node);
+  fieldTemplate = handleJsDoc(fieldTemplate, node);
 
   return fieldTemplate;
 }
--- packages/analyzer/src/features/analyse-phase/creators/createVariable.js
+++ packages/analyzer/src/features/analyse-phase/creators/createVariable.js
@@ -1,6 +1,10 @@
 import ts from 'typescript';
-import { isWellKnownType } from '../../../utils/ast-helpers.js';
-import { handleJsDoc, handleTypeInference } from './handlers.js';
+import {
+  handleExplicitType,
+  handleJsDoc,
+  handleTypeInference,
+  handleWellKnownTypes
+} from './handlers.js';
 
 export function createVariable(variableStatementNode, declarationNode) {
   let variableTemplate = {
@@ -9,15 +13,8 @@ export function createVariable(variableStatementNode, declarationNode) {
   };
 
   variableTemplate = handleTypeInference(variableTemplate, declarationNode);
-
-  if(declarationNode?.type) {
-    variableTemplate.type = { text: declarationNode?.type?.getText() }
-  }
-
-  if (isWellKnownType(declarationNode)) {
-    variableTemplate.type = { text: declarationNode.initializer.expression.getText() };
-  }
-
+  variableTemplate = handleExplicitType(variableTemplate, declarationNode);
+  variableTemplate = handleWellKnownTypes(variableTemplate, declarationNode);
   variableTemplate = handleJsDoc(variableTemplate, variableStatementNode);
 
   return variableTemplate;
