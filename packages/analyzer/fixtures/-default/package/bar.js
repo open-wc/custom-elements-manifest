1d4c7b176ed9e2f4c3ae86b29afca8d37e30db8f
thepassle <pascalschilp@gmail.com>
2021-07-03 12:09:58 +0200

    feat: stencil

--- packages/analyzer/fixtures/-default/package/bar.js
+++ packages/analyzer/fixtures/-default/package/bar.js
@@ -1,11 +1,37 @@
-export default class MyElement extends HTMLElement {
+// TodoList should have `todo-list` as `tagName` in the output custom-elements.json
+// This is also an export of kind: "custom-elements-definition"
+@Component({
+  tag: 'todo-list'
+})
+export class TodoList {
+  // shows up as attr
+  @Prop() color: string;
+  // shows up as `is-valid` attr (camelcase to kebab)
+  @Prop() isValid: boolean;
+  // doesnt show up as attr! (complex type)
+  @Prop() controller: MyController;
+  // shows up as attr `valid`
+  @Prop({ attribute: 'valid' }) isValid: boolean;
+  // shows up as attr `message` (probably doesnt even need special handling, but just incase)
+  @Prop({ reflects: true }) message = 'Hello';
 
-  /** 
-   * @attr my-foo
-   * @reflect
-   */
-  foo = 'asd'
+  // shows up as event `todoCompleted`
+  // `todoCompleted` should not be present in the class's members array
+  @Event() todoCompleted: EventEmitter<Todo>;
+  
+  // shows up as event `foo`
+  @Event({
+    eventName: 'foo',
+  }) fooEvent: EventEmitter<Todo>;
 
+  someMethod(){}
+
+  // these should not show up in custom-elements.json
+  componentWillLoad(){}
+  componentDidLoad(){}
+  componentShouldUpdate(){}
+  componentWillRender(){}
+  componentDidRender(){}
+  componentWillUpdate(){}
+  componentDidUpdate(){}
 }
-
-customElements.define('my-el', MyElement);
