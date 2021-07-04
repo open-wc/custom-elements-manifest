7a3a3e94d7f293eeb59bc3ee098a48c1bb585e2c
thepassle <pascalschilp@gmail.com>
2021-07-04 12:36:29 +0200

    feat: interface inheritance

---
+++ packages/analyzer/fixtures/-default/package/b.js
@@ -0,0 +1,4 @@
+
+export interface F {
+  f: string
+}
--- packages/analyzer/fixtures/-default/package/bar.js
+++ packages/analyzer/fixtures/-default/package/bar.js
@@ -1,19 +1,37 @@
-import type Bar from 'bar';
+import type C from 'bar';
+import F from 'b.js';
+
+export interface G {
+  g: boolean;
+}
+export interface B extends F, G {
+  b: string;
+}
 
 /**
  * Description of the interface
  */
-export interface MyInterface extends Foo, Bar implements Baz {
-  /** 
-   * the name of the class 
-   * @summary this is the summary
-   */
-  name: string;
-
-  /** super classes and mixins */
-  superClasses: SuperClass[];
-
-  member: ClassMember;
-
-  baz(a: string): void
+export interface A extends B, C implements D {
+  a: string;
 }
+
+
+// import type Bar from 'bar';
+
+// /**
+//  * Description of the interface
+//  */
+// export interface MyInterface extends Foo, Bar implements Baz {
+//   /** 
+//    * the name of the class 
+//    * @summary this is the summary
+//    */
+//   name: string;
+
+//   /** super classes and mixins */
+//   superClasses: SuperClass[];
+
+//   member: ClassMember;
+
+//   baz(a: string): void
+// }
