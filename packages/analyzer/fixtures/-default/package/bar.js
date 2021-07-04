f373c5dc7134c65d8d0ffab14d7dd505e7f5c0f6
thepassle <pascalschilp@gmail.com>
2021-07-04 11:19:45 +0200

    feat: add support for interfaces

--- packages/analyzer/fixtures/-default/package/bar.js
+++ packages/analyzer/fixtures/-default/package/bar.js
@@ -1,10 +1,19 @@
-export class MyEl extends LitElement {
-  static get properties() {
-    return {
-      foo: {type: String}
-    }
-  }
+import type Bar from 'bar';
 
-  @property()
-  bar;
+/**
+ * Description of the interface
+ */
+export interface MyInterface extends Foo, Bar implements Baz {
+  /** 
+   * the name of the class 
+   * @summary this is the summary
+   */
+  name: string;
+
+  /** super classes and mixins */
+  superClasses: SuperClass[];
+
+  member: ClassMember;
+
+  baz(a: string): void
 }
