961de54f091f78991b43c9c3504e7d1060cbc209
thepassle <pascalschilp@gmail.com>
2021-07-24 10:10:57 +0200

    fix: two bugfixes

--- packages/analyzer/fixtures/custom-elements-define/package/my-element.js
+++ packages/analyzer/fixtures/custom-elements-define/package/my-element.js
@@ -10,3 +10,7 @@ customElements.define('my-foo', MyFoo);
 customElements.define('my-bar', MyBar);
 customElements.define('my-element', MyElement);
 window.customElements.define('my-window', MyWindow);
+
+customElements.define('anon-1', class extends HTMLElement {});
+
+customElements.define('anon-2', class MyEl extends HTMLElement {});
