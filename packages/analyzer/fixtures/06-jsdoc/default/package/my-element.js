011036116a1ce7b40d72e390addd5b463431a573
konnorrogers <konnor5456@gmail.com>
2024-05-06 16:24:53 -0400

    add tests for @attr / @attribute

--- packages/analyzer/fixtures/06-jsdoc/default/package/my-element.js
+++ packages/analyzer/fixtures/06-jsdoc/default/package/my-element.js
@@ -9,6 +9,34 @@ export class DefaultValues extends HTMLElement {
   defaultWhereCodeTakesPrecedence = 'default from code';
 
   withoutDefault;
+
+  constructor () {
+    super()
+
+    /**
+     * Should work with "@attribute" and no name
+     * @attribute
+     */
+    this.foo = "bar"
+
+    /**
+     * Should work with "@attribute" and a name
+     * @attribute my-foo
+     */
+    this.myFoo = "bar"
+
+    /**
+     * Should work with "@attr" and no name
+     * @attr
+     */
+    this.bar = "bar"
+
+    /**
+     * Should work with "@attr" and a name
+     * @attr my-bar
+     */
+    this.myBar = "bar"
+  }
 }
 
 customElements.define('default-values', DefaultValues);
